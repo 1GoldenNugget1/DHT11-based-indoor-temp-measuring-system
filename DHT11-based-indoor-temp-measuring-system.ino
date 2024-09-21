ebb7f4f3e06ba7c4d9a4f9b2d355f03a7e7bbdbd
1GoldenNugget1 <76992055+1GoldenNugget1@users.noreply.github.com>
2024-09-22 00:17:48 +0200

    initial commit

---
+++ DHT11-based-indoor-temp-measuring-system.ino
@@ -0,0 +1,63 @@
+#include <WiFi.h>
+#include <ESPAsyncWebSrv.h>
+#include <DHT.h>
+
+// Pin configuration for DHT sensor
+#define DHTPIN 4
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+// WiFi credentials
+const char *ssid = "UPC6134271";       // Replace with your network SSID
+const char *password = "vGM7dtunx5sx"; // Replace with your network password
+
+AsyncWebServer server(80); // Create AsyncWebServer object on port 80
+
+void setup() {
+  // Start serial communication for debugging
+  Serial.begin(9600);
+  
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  
+  Serial.println("Connected to WiFi");
+  Serial.println(WiFi.localIP());  // Print local IP address
+  
+  dht.begin();  // Initialize DHT sensor
+  
+  // Define web server route to handle root ("/")
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    float temperature = dht.readTemperature();
+    float humidity = dht.readHumidity();
+    
+    String html = "<html><head><meta charset='utf-8'><meta http-equiv='refresh' content='10' /></head><body><h1>ESP32 DHT11</h1>";
+    html += "<p>Temperatura: " + String(temperature) + " *C</p>";
+    html += "<p>Wilgotność: " + String(humidity) + " %RH</p>";
+    html += "</body></html>";
+    
+    request->send(200, "text/html", html);
+  });
+
+  // Start the web server
+  server.begin();
+}
+
+void loop() {
+  // Reading the DHT sensor data
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+  
+  // Print sensor data to Serial Monitor for debugging
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.print(" %RH | ");
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" *C");
+
+  delay(10000); // Wait 10 seconds between readings
+}
